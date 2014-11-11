97472582ab6415aea0b33b95e5a8f0ba6e6f6def
Andro <akrezic@hotmail.com>
2014-11-11 11:17:20 +0100

    Implemented two adapters for "My places" and "Near me - listview", added some test data.

---
+++ app/src/main/java/com/zimmerfrei/air/zimmerfrei/MyPlacesAdapter.java
@@ -0,0 +1,7 @@
+package com.zimmerfrei.air.zimmerfrei;
+
+/**
+ * Created by Andro on 7.11.2014..
+ */
+public class MyPlacesAdapter {
+}
---
+++ app/src/main/java/com/zimmerfrei/air/zimmerfrei/NearMeListAdapter.java
@@ -0,0 +1,7 @@
+package com.zimmerfrei.air.zimmerfrei;
+
+/**
+ * Created by Andro on 10.11.2014..
+ */
+public class NearMeListAdapter {
+}
